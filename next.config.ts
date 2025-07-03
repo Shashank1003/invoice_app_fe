a19cd32c9f62cd4d93f7eaa0d63072ed68b03cd0
Shashank1003 <guptashashank8975@gmail.com>
2025-07-03 21:58:47 +0530

    fix build issues
    Signed-off-by: Shashank1003 <guptashashank8975@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 // ✅ Proper TS-style config
-const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
+import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
+
+const nextConfig: NextConfig = {
+    webpack(config: Configuration) {
+        config.module?.rules?.push({
             test: /\.svg$/i,
             issuer: /\.[jt]sx?$/,
             use: ["@svgr/webpack"],
