3cc9354680767c46de48cbe6072e25f339bb5d4d
Shashank1003 <guptashashank8975@gmail.com>
2025-07-13 16:47:33 +0530

    add md design for delete invoice modal
    Signed-off-by: Shashank1003 <guptashashank8975@gmail.com>

--- src/components/invoice/DeleteConfirmation.tsx
+++ src/components/invoice/DeleteConfirmation.tsx
@@ -12,30 +12,30 @@ export default function DeleteConfirmation({
     useLockScroll(true);
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/75 px-[24px]">
-            <div className="bg-secondary-bg shadow-custom rounded-[8px] p-[32px]">
-                <p className="text-text text-[20px] leading-[32px] font-bold tracking-[-0.42px]">
+        <div className="fixed inset-0 flex items-center justify-center bg-black/75 px-6">
+            <div className="bg-secondary-bg shadow-custom rounded-[8px] p-8 md:p-12">
+                <p className="text-text text-[20px] leading-[32px] font-bold tracking-[-0.42px] md:text-[24px] md:leading-[-0.5px]">
                     Confirm Deletion
                 </p>
 
-                <p className="text-gray-steel text- mt-[8px] text-[12px] leading-[22px] font-medium tracking-[-0.25px]">
+                <p className="text-gray-steel mt-2 text-[12px] leading-[22px] font-medium tracking-[-0.25px] md:mt-3">
                     Are you sure you want to delete invoice #XM9141? This action
                     cannot be undone.
                 </p>
 
-                <div className="mt-[24px] flex items-center justify-end gap-[8px]">
+                <div className="mt-6 flex items-center justify-end gap-2 md:mt-4">
                     <CustomButton
                         variant="button3"
                         buttonText="Cancel"
                         onClick={onCancel}
-                        style={{ width: "90px" }}
+                        extendedClass="!w-[90px]"
                     />
 
                     <CustomButton
                         variant="redButton"
                         buttonText="Delete"
                         onClick={onDelete}
-                        style={{ width: "90px" }}
+                        extendedClass="!w-[90px]"
                     />
                 </div>
             </div>
