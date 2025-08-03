b0eeb414f8c358cbe3327992d32a31cc91f7b9d5
Shashank1003 <guptashashank8975@gmail.com>
2025-08-03 14:00:27 +0530

    minor color fix
    Signed-off-by: Shashank1003 <guptashashank8975@gmail.com>

--- src/components/invoices/InvoiceCardMedium.tsx
+++ src/components/invoices/InvoiceCardMedium.tsx
@@ -40,7 +40,7 @@ export default function InvoiceCardMedium({
             <div className="flex items-center justify-between">
                 <StatusBox invoiceStatus={invoice.status} />
 
-                <RightArrowIcon />
+                <RightArrowIcon className="text-indigo-primary" />
             </div>
         </div>
     );
